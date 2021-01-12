5fa5c0a6b4a3080ab884a7abac3cdcde8a17b58b
Carlos <cvictor.rsouza@gmail.com>
2021-01-12 08:40:34 -0300

    .env added for deploy

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://www.omdbapi.com/?apikey=925eba28&s=",
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 export default api;
